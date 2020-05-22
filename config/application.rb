1488390929b8e4a3d44bf8f72065a6e282ff58f1
miyatayo0228 <beebees10c@gmail.com>
2020-05-22 13:20:54 +0900

    top画面の実装

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Costcheck
   class Application < Rails::Application
-    config.generators do |g|
-      g.stylesheets false
-      g.javascripts false
-      g.helper false
-      g.test_framework false
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
