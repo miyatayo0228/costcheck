d8bb86008d2b4e30125178fd1ad5c1f6afa2df09
miyatayo0228 <beebees10c@gmail.com>
2020-05-22 19:26:49 +0900

    ユーザー登録、ログイン機能実装

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,9 +1,14 @@
 class TopsController < ApplicationController
   
+  before_action :move_to_index, except: [:index, :show]
+  
   def index
   end
   
   def show
   end
 
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
