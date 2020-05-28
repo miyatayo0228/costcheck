a9add21c0d4897991d9daad12258bea9e8b2fb70
miyatayo0228 <beebees10c@gmail.com>
2020-05-28 12:16:56 +0900

    ゲストログイン機能実装

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,12 +1,22 @@
 class TopsController < ApplicationController
   
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new_guest]
   
   def index
   end
   
   def show
   end
+
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = "GuestUser"
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
